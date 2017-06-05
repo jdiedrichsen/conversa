919c2361f3e2f6329d79b659432d792bef9776bb
ShaynAli <shayaan.syed.ali@gmail.com>
2017-06-05 10:57:44 -0400

    Set pid and cam to vectors

--- cnv_loadLabelFile.m
+++ cnv_loadLabelFile.m
@@ -35,8 +35,21 @@ FIELD_MAP = containers.Map( ...
 
 dlf = dload(filename); % dlf for dloadFile
 
-cnv_loadLabelFile.pid = dlf.pid(1); % Set pid
-cnv_loadLabelFile.cam = dlf.cam(1); % Set cam number
+if (isfield(fld,'ms')) % Decode by using milliseconds
+    
+elseif (isfield(fld,'Frame')) % Decode by using frame numbers
+    
+else % Error - neither frames nor milliseconds provided
+    error('Label file does not contain milliseconds or frame numbers');
+end;
+
+nEntries = length(dlf.pid);
+% Set pid and cam as single entry
+% cnv_loadLabelFile.pid = dlf.pid(1); % Set pid
+% cnv_loadLabelFile.cam = dlf.cam(1); % Set cam number
+% Set pid and cam as array of entries
+cnv_loadLabelFile.pid = repmat(dlf.pid(1), 1, nEntries)'; % Set pid array (does not vary)
+cnv_loadLabelFile.cam = repmat(dlf.cam(1), 1, nEntries)'; % Set cam number array (does not vary)
 
 behaviourFields = {};
 dlfFields = fieldnames(dlf);
