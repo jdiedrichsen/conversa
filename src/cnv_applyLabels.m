0929c2b8d27c58113490576c402c20d90088a653
ShaynAli <shayaan.syed.ali@gmail.com>
2017-06-08 15:14:00 -0400

    Fixed label file and finished applyLabels

--- src/cnv_applyLabels.m
+++ src/cnv_applyLabels.m
@@ -1,10 +1,37 @@
-function varargout = cnv_applyLabels(trackingData, labels)
+function labelledDf = cnv_applyLabels(trackingData, labels)
+% TODO: Documentation
 
 excludeFields = {'timestamp', 'istracked', 'bodyid'};
 
+% Add trackingData fields after they start tracking (after first point of
+% change)
 start = firstChangeI(trackingData, 'exclude', excludeFields);
+trackingFields = fieldnames(trackingData);
+nTrackingFields = length(trackingFields);
+for i = 1:nTrackingFields
+    fieldName = trackingFields{i};
+    labelledDf.(fieldName) = trackingData.(fieldName)(start:end);
+end;
+% Zero timestamps to true start
+labelledDf.timestamp = labelledDf.timestamp - labelledDf.timestamp(1);
+% There seems to be additional time adjustments required here as the
+% timestamps currently have a length difference of almost 1 second
+nFrames = length(labelledDf.timestamp);
 
-end
+% Add labels
+% Set all values to false then update from labels
+nBehavs = length(labels.behaviour);
+for i = 1:nBehavs
+    behavName = labels.behaviour{i};
+    labelledDf.(behavName)(1:nFrames, 1) = false;
+end;
+for i = 1:nBehavs
+    behavName = labels.behaviour{i};
+    startI = timestampIndex(labels.start(i));
+    endI = timestampIndex(labels.end(i));
+    labelledDf.(behavName)(startI:endI, 1) = true;
+end;
+end % cnv_applyLabels
 
 % Finds first point index of change in a data struct's fields
 function changeI = firstChangeI(data, varargin)
@@ -43,3 +70,7 @@ end;
 changeI = -1;
 return ;  % No change found
 end % firstChangeI
+
+function index = timestampIndex(time)
+    index = round(30*time + 1);
+end
