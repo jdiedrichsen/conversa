2f348b5e28fffba249f780cc06930b2fdd33b973
ShaynAli <shayaan.syed.ali@gmail.com>
2017-06-25 10:04:54 -0400

    Beginning reffactoring process and completing struct2Matrix

--- src/cnv_struct2Matrix.m
+++ src/cnv_struct2Matrix.m
@@ -1,5 +1,25 @@
-function out = cnv_struct2Matrix(inStruct)
+function out = cnv_struct2Matrix(inStruct, varargin)
+% Converts a struct to a matrix
+% Varargin options:
+%	includefields
+%		A cell list of fields to include or the string 'all' to include all
+%		fields, 'all' is assumed by default
+%	excludefields
+%		A cell list of fields to exclude. Assumes an empty list by default
 
+defaultOptions = struct( ...
+	'includefields', 'all', ...
+	'excludefields', {} ...
+	);
 
+optionArgs = cnv_getArgs(defaultOptions, varargin);
+
+% Add 
+fields = fieldname(inStruct);
+if(ischar(optionArgs.includefields) && strcmp(optionArgs.includefields, 'all')) % Use all fields in 'all' case
+	
+else
+	
+end;
 
 end
