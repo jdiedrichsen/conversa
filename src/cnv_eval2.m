8f37c98881e6107f1d9f7713be6266c7c73c46e2
ShaynAli <shayaan.syed.ali@gmail.com>
2017-06-27 13:44:00 -0400

    Updated description of eval2

--- src/cnv_eval2.m
+++ src/cnv_eval2.m
@@ -1,9 +1,21 @@
 function out = cnv_eval2(data, algoNames, targetFields, varargin)
 % Evaluates learning algorithms
-% Takes a matrix of predictors, array of labels, and cell array of the
-% names of the algorithms to evaluate
-% Returns an array of error rates for each algorithm in algoNames
+% 
+% Takes a data struct with labelled training data, a cell array of the
+% names of the algorithms to be tested, and a few optional argument
+% parameters
+% 
+% Assumes that each algorithm name in algoNames is accompanied by a
+% Matlab script with the prefix cnv_learn_algoNames{i} and
+% cnv_predict_algoNames{i} for learning and prediction
+% (classification/regression) respectively
+% 
+% Returns a struct of error rates for each algorithm in algoNames
+% 
 % By Shayaan Syed Ali
-% Last updated 19-Jun-17
+% Last updated 27-Jun-17
+
+
+
 
 end % cnv_eval2
