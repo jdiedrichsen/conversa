d19d055e1b8117790fb326e34ed0548dcec51e56
ShaynAli <shayaan.syed.ali@gmail.com>
2017-07-25 15:39:17 -0400

    Work on LSTM and added Erin's data

--- src/cnv_lstm.py
+++ src/cnv_lstm.py
@@ -1,8 +1,21 @@
+# Imports
+
 # Tensorflow import
 import tensorflow as tf
 # Keras imports
-from keras.models import  Sequential
-from keras.models import LSTM
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+# Numpy imports
+import numpy as mp
+
+# Constants
+
+data_dim = 32  # Each time contains 32 pieces of tracking data
+timesteps = 8  # Total number of times to consider
+n_classes = 2  # Binary classification, either smiling or not, talking or not, etc.
+
+# Set up model architecture
 
 model = Sequential()
-
+# Includes 32 features (e.g. smile_l, jow_open_l, etc.) but excludes timestamp
+model.add(LSTM(32, return_sequences=True, input_shape=(timesteps, data_dim)))
