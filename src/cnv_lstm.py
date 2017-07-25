2b54d41b4adb5b669a2dd6401fc0e401a152a355
ShaynAli <shayaan.syed.ali@gmail.com>
2017-07-25 16:00:05 -0400

    First pass of LSTM, untested and not eval compliant

--- src/cnv_lstm.py
+++ src/cnv_lstm.py
@@ -11,11 +11,21 @@ import numpy as mp
 # Constants
 
 data_dim = 32  # Each time contains 32 pieces of tracking data
+layer_dim = 32  # Number of units in each layer
+output_dim = 1  # Output dimension
 timesteps = 8  # Total number of times to consider
-n_classes = 2  # Binary classification, either smiling or not, talking or not, etc.
+n_classes = 1  # Binary classification, either smiling or not, talking or not, etc.
 
 # Set up model architecture
 
 model = Sequential()
-# Includes 32 features (e.g. smile_l, jow_open_l, etc.) but excludes timestamp
-model.add(LSTM(32, return_sequences=True, input_shape=(timesteps, data_dim)))
+# Input layer
+model.add(LSTM(layer_dim,
+               return_sequences=True,
+               input_shape=(timesteps, data_dim)))
+# Hidden layers
+model.add(LSTM(layer_dim,
+               return_sequences=True))
+model.add(LSTM(layer_dim))  # Returns a single vector of dimension layer_dim
+# Output layer
+model.add(Dense(output_dim, activation ='softmax'))
