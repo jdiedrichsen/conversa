92830c1317aed9edd92708e231ff1da83d93c060
ShaynAli <shayaan.syed.ali@gmail.com>
2017-08-17 11:45:35 -0400

    Starting to change eval_models to pandas DF

--- src/cnv_eval.py
+++ src/cnv_eval.py
@@ -10,6 +10,7 @@ __status__ = 'Development'
 # __version__ = ''
 
 import numpy as np
+import pandas as pd
 
 # TODO: Add verbose flags and vprint function
 # TODO: mean prediction and LDA (and Naive Bayes?)
@@ -28,33 +29,36 @@ def eval_models(models,
                 n_folds=5,
                 train_n_epochs=10,
                 train_batch_sz=10,
-                test_n_epochs=10,
-                test_n_batch_sz=10):
+                test_n_batch_sz=1,
+                verbose=0):
     '''
     Evaluates models given predictor and label data to train and test the models on
-    :param models: 
-    :param predictors: 
-    :param labels: 
-    :param n_folds: 
-    :param train_n_epochs: 
-    :param train_batch_sz: 
-    :param test_n_epochs: 
-    :param test_n_batch_sz: 
+    :param models: The models to evaluate
+    :param predictors: Predictors to test the models on
+    :param labels: Labels to test the models on
+    :param n_folds: The number of folds to test the data on, defaults to 5
+    :param train_n_epochs: The number of passes each models gets on the data, defaults to 10
+    :param train_batch_sz: The number of data points to give each model on at once, defaults to 10
+    :param test_n_batch_sz: The number of data points to test each model on at once, defaults to 1
+    :param verbose: The verbosity level of training and testing - note that model console output often conflicts with 
+    outputs from cnv_eval - defaults to 0 (not verbose)
     :return: 
     '''
+    # TODO: Change return to pandas DataFrame
     folds = k_fold(predictors, labels, n_folds)
+    pd.DataFrame()
     accuracies = []
     for model in models:
-        print('\n\nMoving to next model')
+        print('\nMoving to next model')
         for (train_data, test_data) in folds:
             print('\nMoving to next fold')
             (train_predictors, train_labels) = train_data
             (test_predictors, test_labels) = test_data
             # Train
-            model.fit(train_predictors, train_labels, epochs=train_n_epochs, batch_size=train_batch_sz, verbose=0)
+            model.fit(train_predictors, train_labels, epochs=train_n_epochs, batch_size=train_batch_sz, verbose=verbose)
             # Test
-            scores = model.evaluate(test_predictors, test_labels, batch_size=test_n_batch_sz, verbose=0)
-            accuracies.append(scores)  # TODO: Set to structured numpy array, fieldnames as spec_model names
+            (loss, accuracy) = model.evaluate(test_predictors, test_labels, batch_size=test_n_batch_sz, verbose=verbose)
+            accuracies.append(accuracy)
     return accuracies
 
 
@@ -67,6 +71,7 @@ def k_fold(predictors, labels, n_folds):
     :return: Each fold is a nested tuple, of (train_data, test_data) where
     train_data = (train_predictors, train_labels) and test_data = (test_predictors, test_labels)
     '''
+    # TODO: Change to pandas DataFrame
     folds = list()
     for i in range(0, n_folds):
         test_data = (
